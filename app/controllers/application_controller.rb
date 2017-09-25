66bcf1da6ca0649be46843562ad32180d5335a98
EnasGaberMostafa <enassays2@gmail.com>
2017-09-25 11:45:29 +0200

    Add devise and styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user! # before accessing app you must sign in user
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index] # skip this part in case of index
   def index
   end
 end
