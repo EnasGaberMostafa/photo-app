66c3515c70c27afedddd23f8a6ca1c3241c9a16c
EnasGaberMostafa <enassays2@gmail.com>
2017-09-25 12:37:43 +0200

    Setup email functionality for sign-up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,11 @@
+Rails.application.routes.default_url_options[:host] = "localhost:3000"
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
+  
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
